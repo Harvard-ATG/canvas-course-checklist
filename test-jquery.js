e871691e22357942692fb0cc5241838382564351
Arthur Barrett <abarrett@fas.harvard.edu>
2015-05-28 16:46:37 -0400

    Updated jquery test.

--- test-jquery.js
+++ test-jquery.js
@@ -6,10 +6,35 @@ require(['jquery'], function($) {
 		},
 		updateWizard: function() {
 			console.log("Updating Wizard...");
-			//this.remove('wizard_add_students');
+			this.remove('wizard_add_students');
+			this.replaceText("wizard_add_tas", /^TA(s?)/, "TF$1");
 		},
 		remove: function(id) {
-			$(id).remove();
+			$("#"+id).remove();
+		},
+		replaceText: function(id, pattern, replacement) {
+			var $itemEl = $("#"+id); 
+			var $boxEl = this.getMessageBoxEl();
+			var that = this;
+
+			// Replace text on the checklist 
+			that._replaceText($itemEl, pattern, replacement);
+
+			// When the checklist item is clicked, replace text in the message box
+			$itemEl.on("click", function(e) {
+				that._replaceText($boxEl, pattern, replacement);
+			});
+		},
+		_replaceText: function($el, pattern, replacement) {
+			// Replace all descendant text node values
+			$el.find("*").contents().filter(function () { 
+				return this.nodeType === 3; 
+			}).each(function() {
+				this.nodeValue = this.nodeValue.replace(pattern, replacement);
+			});
+		},
+		getMessageBoxEl: function() {
+			return $(".CourseWizard__modalOverlay .ic-wizard-box__message");
 		}
 	};
 
