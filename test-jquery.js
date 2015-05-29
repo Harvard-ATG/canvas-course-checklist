3ef2225f5fe4d0213c0d5e3fd24bda775d55eef7
Arthur Barrett <abarrett@fas.harvard.edu>
2015-05-29 17:28:54 -0400

    Customizations almost working.

--- test-jquery.js
+++ test-jquery.js
@@ -1,83 +1,115 @@
-require(['jquery'], function($) {
+require([
+	'jquery',
+	'react',
+	'jsx/course_wizard/ListItems',
+	'jsx/course_wizard/ChecklistItem',
+	'jsx/course_wizard/CourseWizard'
+], function($, React, ListItems, ChecklistItem, CourseWizard) {
 	
 	/**
-	 * See the CourseWizard ListItems JSX file and the Courses Controller for the environment variables passed to the JS ENV:
+	 * The CourseWizard (i.e. Setup Checklist) is a javascript component built using ReactJS.
+	 * If you're not familiar with React, it uses a syntax extension called JSX, which is compiled
+	 * down to JS on the server. The CourseWizard is composed of several sub-components, each of
+	 * which is contained in a separate JSX file.
 	 *
+	 * The JSX files are passed environment values from the Courses Controller, which are set in the
+	 * global ENV namespace. Refer to the source files below:
+	 * 
 	 * 1) https://github.com/instructure/canvas-lms/blob/master/app/jsx/course_wizard/ListItems.jsx
-	 * 2) https://github.com/instructure/canvas-lms/blob/master/app/controllers/courses_controller.rb
-	 */
-	var ADD_TFS_URL = ENV.COURSE_WIZARD.urls.add_tas;
-
-	/**
-	 * Customizes the appearance of the "Setup Checklist" on the Course home 
-	 * page (for instructors only).
+	 * 2) https://github.com/instructure/canvas-lms/blob/master/app/jsx/course_wizard/ChecklistItem.jsx
+	 * 3) https://github.com/instructure/canvas-lms/blob/master/app/jsx/course_wizard/CourseWizard.jsx
+	 * 4) https://github.com/instructure/canvas-lms/blob/master/app/controllers/courses_controller.rb
+     *
+	 * In order to customize the CourseWizard, we need to unmount the component and disable the existing
+	 * event handler on the "Setup Checklist" button. Then, we need to pull in the React components,
+	 * customize the ListItems and override the method that renders those items, and then re-initialize
+	 * the button that renders the top-level CourseWizard React component.
 	 *
-	 * NOTE: 
-	 * The course wizard is a ReactJS component, and this makes
-	 * it somewhat difficult to modify the wizard because React expects
-	 * to manage the DOM (React implements its own "shadow DOM" to know
-	 * when/how to update DOM elements). For background on ReactJS,
-	 * see: https://facebook.github.io/react/
+	 * You might be wondering why all of this is necessary. Why not just write some jQuery code to
+	 * grab the DOM elements and change them at will? The reason is: React. React expects to completely
+	 * manage the DOM under the root node, and has a sophisticated algorithm for changing the DOM when
+	 * there are state changes (it maintains its own shadow DOM and performs a diff to know what to
+	 * change and when to change it). This makes it rather complicated to change things out from under
+	 * React, and expect the React component to still work as intended. Efforts to do so only partially
+	 * worked. So while this approach seems more complicated than it needs to be, it is in fact a "safer"
+	 * approach since we can be sure the React component is working properly.
 	 *
+	 * The bottom line is that Instructure has not made this component very flexible or open to
+	 * modification, so at present, it's the best we can do, assuming we don't want to completely
+	 * disable their version of the CourseWizard and write our own from scratch.
 	 */
-	var Customize = {
-		// Event handler for when the "Setup Checklist" button is clicked
-		onClick: function(e) {
-			console.log("Clicked Wizard", "event", e, "this", this);
-			this.customize();
-		},
-		customize: function() {
-			this.removeItem('wizard_add_students');
 
-			this.updateItem({
-				id: "wizard_add_tas", 
-				text: "Add TFs to the Course", 
-				msg: [
-					"You may want to assign some TFs to help you with the course. ",
-					"TFs can grade student submissions, help moderate the discussions ",
-					"and even update due dates and assignment details for you."
-				].join(''),
-				btn: '<a href="'+ADD_TFS_URL+'" class="Button Button--primary">Add TFs to the Course</a>'
-			});
-		},
-		// Removes an item from the checklist
-		removeItem: function(id) {
-			console.log("remove", id);
-			$("#"+id).remove();
-		},
-		// Updates an item in the checklist 
-		updateItem: function(item) {
-			console.log("update", item);
-			var that = this;
-			var $el = $("#"+item.id);
-			$el.text(item.text);
-			this.getNavEl().on("click", function(e) {
-				if (!$(e.target).is($el)) {
-					return true;
-				}
-				var $box = that.getMessageBoxEl();
-				console.log("clicked", e, $box, item.msg, item.btn);
-				$box.find('.ic-wizard-box__message-text').text(item.msg);
-				$box.find('.ic-wizard-box__message-button').html(item.btn);
-				return false;
-			});
-		},
-		// Returns the message box container element
-		getMessageBoxEl: function() {
-			return $(".CourseWizard__modalOverlay .ic-wizard-box__message");
-		},
-		getNavEl: function() {
-			return $(".ic-wizard-box__nav");
-		},
-		getModalEl: function() {
-			return $(".ReactModalPortal");
-		}
-	};
+	var CustomizedListItems = (function(items) {
 
-	// Bind the context of the event handler to the object it is attached to
-	Customize.onClick = $.proxy(Customize.onClick, Customize);
+		// REMOVE: Add Students (third item -- at index 2 in the array)
+		items.splice(2, 1);
+		
+		// CHANGE: TA => TF in title and text
+		$.each(['text', 'title'], function(idx, prop) {
+			var wizard_add_tas = items[6];
+			wizard_add_tas[prop] = wizard_add_tas[prop].replace(/TA(s)?/g, "TF$1");
+		});
+		
+		// INSERT: Academic Integrity Policy
+		items.splice(7, 0, {
+			key:'policy_wizard',
+			complete: false,
+			title: "Customize academic integrity policy",
+			text: "Customize the academic integrity policy for your course.",
+			url: "/courses/39/external_tools/1513",
+			iconClass: 'icon-educators'
+		});
 
-	// Attach our customizations to the wizard button
-	console.log("setting up jquery test...", Customize);
-	$(".wizard_popup_link").on("click", Customize.onClick);
+		return items;
+	})(ListItems); 
+
+/*
+	var $popup_link = $(".wizard_popup_link");
+	var $wizard_box = $("#wizard_box");
+	var courseWizardFactory = React.createFactory(CourseWizard);
+
+	React.unmountComponentAtNode($wizard_box[0]);
+
+	$popup_link.off("click");
+
+	$popup_link.on("click", function(event) {
+		var component = React.render(courseWizardFactory({
+			overlayClassName: 'CourseWizard__modalOverlay',
+			showWizard: true
+		}), $wizard_box[0]);
+
+		var checklist_component = component.refs.wizardBox.props.children[0].props.children[1];
+		
+		var saved_render = checklist_component.render;
+		
+		checklist_component.render = function() {
+			console.log("render called for checklist component");
+			return saved_render.apply(this, arguments);
+		};
+		
+		checklist_component.renderCheckList = function() {
+			console.log("render checklist", this, CustomizedListItems);
+			var result = CustomizedListItems.map(function(item) {
+				console.log("mapped", item.title, item.id);
+				var isSelected = (this.state.selectedItem === item.key);
+				var id = "wizard_" + item.key;
+				return ChecklistItem({
+					complete: item.complete,
+					id: id,
+					key: item.key,
+					stepKey: item.key,
+					title: item.title,
+					onClick: this.props.clickHandler,
+					isSelected: isSelected
+				})
+			}.bind(this))
+			
+			console.log("result", result);
+
+			return result;
+		};
+
+		//checklist_component.render();
+	});
+*/
 });
