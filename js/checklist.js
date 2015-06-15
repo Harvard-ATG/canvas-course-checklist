ac046f20088d96e01c26c38ea9dc3828305a54c5
Arthur Barrett <abarrett@fas.harvard.edu>
2015-06-15 12:07:03 -0400

    Debug.

--- js/checklist.js
+++ js/checklist.js
@@ -104,7 +104,7 @@ require(['jquery', 'jsx/course_wizard/ListItems'], function($, ListItems) {
 
 	//----- CHANGE #1 -----
 	// REMOVE: Modify "Import Content" item
-	ListItems[0].text = "If you've been using another course management system, you probably have stuff in there that you're going to want moved over to Canvas. We can walk you through the process of easily migrating your content into Canvas. If you would like to incorporate content from a previous iSite, please contact the Academic Technology Group at <a href=\"mailto:atg@fas.harvard.edu\">atg@fas.harvard.edu</a>";
+	ListItems[0].text = "If you've been using another course management system, you probably have stuff in there that you're going to want moved over to Canvas. We can walk you through the process of easily migrating your content into Canvas. If you would like to incorporate content from a previous iSite, please contact the Academic Technology Group at atg@fas.harvard.edu";
 	
 	//----- CHANGE #2 -----
 	// REMOVE: "Add Students" item
